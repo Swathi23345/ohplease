0130fd5b9232aff17ce4c58a87d35df1ddd44e06
SwathiPriyaRV <66841534+Swathi23345@users.noreply.github.com>
2023-08-10 03:17:41 +0530

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -10,11 +10,11 @@ for key, value in json_data.items():
     table_content += f"| {key} | {value} |\n"
 
 # Read and update README
-with open('README.md', 'r') as readme_file:
+with open('test.md', 'r') as readme_file:
     readme_content = readme_file.read()
 
 # Replace placeholder with table content
 readme_content = readme_content.replace('<!-- JSON_TABLE -->', table_content)
 
-with open('README.md', 'w') as readme_file:
+with open('test.md', 'w') as readme_file:
     readme_file.write(readme_content)
