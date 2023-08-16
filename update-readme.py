0e4c036b34fbaa57e942dedfa7de94eccf96d5ac
SwathiPriyaRV <66841534+Swathi23345@users.noreply.github.com>
2023-08-17 00:22:24 +0530

    Update update-readme.py

--- update-readme.py
+++ update-readme.py
@@ -19,9 +19,32 @@ for item in runyaml:
     markdown_table += f"| {item['id']} | {item['name']}  | {item['conclusion']} | {item['run_attempt']} | \n"
 
 # Write Markdown table to file
-with open("test.md", "w") as md_file:
-    md_file.write(markdown_table)
-output = ""
-with open("test.md", "r") as readme_file:
-    output = readme_file.read()
+# with open("test.md", "w") as md_file:
+#     md_file.write(markdown_table)
+# output = ""
+# with open("test.md", "r") as readme_file:
+#     output = readme_file.read()
+
+# update_readme.py
+
+def generate_markdown():
+    # Replace this with your actual code to generate Markdown
+    return markdown_table
+
+def update_readme(readme_path, marker):
+    generated_markdown = generate_markdown()
+
+    with open(readme_path, "r") as readme_file:
+        readme_content = readme_file.read()
+
+    updated_readme_content = readme_content.replace(marker, generated_markdown)
+
+    with open(readme_path, "w") as readme_file:
+        readme_file.write(updated_readme_content)
+
+if __name__ == "__main__":
+    readme_file_path = "README.md"
+    content_marker = "<!-- GENERATED_CONTENT_MARKER -->"
+    update_readme(readme_file_path, content_marker)
+
 
